4dde35c8350bc5b594f086e42c6b55aa9f2e66c5
我是真的猛 <2388995454@qq.com>
2020-11-25 21:22:03 +0800

    111

---
+++ src/com/atguigu/juc/lockaangsyn/sisuo.java
@@ -0,0 +1,30 @@
+package com.atguigu.juc.lockaangsyn;
+
+import java.util.concurrent.TimeUnit;
+
+public class sisuo {
+
+    public static void main(String[] args) {
+
+        Object o1 = new Object();
+        Object o2 = new Object();
+        new Thread(()->{
+            synchronized (o1){
+                System.out.println("A线程拿到了o1，想拿o2");
+                try { TimeUnit.SECONDS.sleep(3); } catch (InterruptedException e) { e.printStackTrace(); }
+                synchronized (o2){
+                    System.out.println("A线程拿到了o2");
+                }
+            }
+        },"A").start();
+        new Thread(()->{
+            synchronized (o2){
+                System.out.println("B线程拿到了o2，想拿o1");
+                try { TimeUnit.SECONDS.sleep(3); } catch (InterruptedException e) { e.printStackTrace(); }
+                synchronized (o1){
+                    System.out.println("B线程拿到了o1");
+                }
+            }
+        },"B").start();
+    }
+}
